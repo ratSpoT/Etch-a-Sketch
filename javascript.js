b661e7c4280c8e62d7fe1a1b835e6436b0069bea
Ling Kai <94221160+ratSpoT@users.noreply.github.com>
2025-09-04 16:49:02 +0800

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,8 @@
+for (let i=0; i<16; i++){
+  let row = document.createElement("div");
+  row.class = "row";
+  for (let j=0; j<16; j++) {
+    let box = document.createElement("div");
+    box.class = "box";
+  }
+}
