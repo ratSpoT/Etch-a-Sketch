ebbefe1e1dcfeb8804d5d617d59828a889e1d919
Ling Kai <94221160+ratSpoT@users.noreply.github.com>
2025-09-04 16:58:44 +0800

    Update javascript.js
    fixed errors in class definition and added appendChild

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
+let container = document.querySelector("div.container");
+
 for (let i=0; i<16; i++){
   let row = document.createElement("div");
-  row.class = "row";
+  row.classList.add("row");
   for (let j=0; j<16; j++) {
     let box = document.createElement("div");
-    box.class = "box";
+    box.classList.add("box");
+    row.appendChild(box);
   }
+  container.appendChild(row);
 }
